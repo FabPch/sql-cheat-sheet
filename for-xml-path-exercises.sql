8891a662eccf9dc171a00f8b4f85d9a91a974da3
FAb <fabien191@hotmail.com>
2023-02-24 21:35:30 +0100

    Add files via upload

---
+++ for-xml-path-exercises.sql
@@ -0,0 +1,12 @@
+SELECT SubcategoryName = A.[Name]
+  , Products = 
+    STUFF(
+	  (
+	    SELECT ';' + B.[Name]
+	    FROM Production.Product B
+	    WHERE A.ProductSubcategoryID = B.ProductSubcategoryID
+		  AND B.ListPrice > 50
+	    FOR XML PATH ('')
+	  )
+	  , 1, 1, '')
+FROM Production.ProductSubcategory A
